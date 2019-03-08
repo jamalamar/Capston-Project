8711c803104c097f94661fcfdc79273d2a21d476
jamalamar <jamal_ab22@hotmail.com>
2019-03-08 16:14:54 -0600

    landing page and login modal working

---
+++ src/views/LandingPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import SimpleModalWrapped from '../Components/modal.js'
+import LoginPage from "../Components/LoginPage.js";
+
+
+class LandingPage extends Component {
+	render(){
+		return(
+			<div>
+				<SimpleModalWrapped/>
+			</div>
+		)
+	}
+}
+
+export default LandingPage;
